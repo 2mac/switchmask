e7d533af8b6c13ded6000f4e8146b784e9904988
David McMackins II <contact@mcmackins.org>
2015-03-24 20:30:51 -0500

    Added function outline

---
+++ switchmask.py
@@ -0,0 +1,36 @@
+##
+##  SwitchMask - Manage your identity.
+##  Copyright (C) 2015 David McMackins II
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU Affero General Public License as published by
+##  the Free Software Foundation, version 3 only.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU Affero General Public License for more details.
+##
+##  You should have received a copy of the GNU Affero General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+__module_name__ = "SwitchMask"
+__module_version__ = "0.0"
+__module_description__ = "Roleplaying nick switcher"
+__module_author__ = "David McMackins II"
+
+import hexchat
+
+masks = {}
+
+def add_mask(name):
+    pass
+
+def remove_mask():
+    pass
+
+def init():
+    pass
+
+init()
